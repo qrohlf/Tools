16e3b5231e8406d04073564c13b0e20838a7e4ad
Quinn Rohlf <qr@qrohlf.com>
2014-09-11 09:22:06 -0700

    update script works!

--- update.rb
+++ update.rb
@@ -1,26 +1,28 @@
 #!/usr/bin/env ruby
-require 'pry'
 
-# Item pattern
+# item definition
 def I(l)
-    l.match /^- .*$/
+    l.match(/^- .*$/)
 end
 
-readme = File.readlines('Readme.md')
-readme_header = []
-readme_footer = []
+# Sort the readme
+readme = File.read('Readme.md')
+readme_sections = readme.split("\n\n")
+readme_sections.map! do |section|
+  lines = section.split("\n")
+  heading = lines.shift
+  if /"^## "/ =~ heading
+    ([heading] + lines.sort).join("\n")
+  else
+    ([heading] + lines).join("\n")
+  end
+end
+File.write('Readme.md', readme_sections.join("\n\n")+"\n")
+
+# Update the sink
 sink = File.readlines('kitchensink.md')
-
-until /^##/ =~ readme.first
-  readme_header << readme.shift
-end
-
-loop do
-  readme_footer.unshift readme.pop
-  break if /^---/ =~ readme_footer.first
-end
-
-readme_sections = readme.group_by{|l| /^##/ =~ l}
-
-binding.pry
-# readme_sections = readme.split '##'
+sink_header = sink.take(sink.index{|l| I(l)})
+sink_footer = sink.slice(sink.rindex{|l| I(l)} + 1, sink.size)
+all = File.readlines('Readme.md') + File.readlines('kitchensink.md')
+all.select!{|l| I(l)}
+File.write('kitchensink.md', (sink_header + all.uniq.sort + sink_footer).join(''))
